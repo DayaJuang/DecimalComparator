3f1a04fd2ec4888d9622e5cb9f825546fe17c473
daya <dayajuangmahaputra@gmail.com>
2022-11-08 11:18:02 +0900

    init commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,26 @@
-package org.example;public class Main {
+package org.example;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean ans = areEqualByThreeDecimalPlaces(-3.175656, -3.175);
+
+        System.out.println(ans);
+    }
+
+    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2){
+        double ans1, ans2;
+
+        BigDecimal bd = new BigDecimal(Double.toString(num1));
+        bd = bd.setScale(3, RoundingMode.DOWN);
+        ans1 = bd.doubleValue();
+
+        bd = new BigDecimal(Double.toString(num2));
+        bd = bd.setScale(3,RoundingMode.DOWN);
+        ans2 = bd.doubleValue();
+
+        return (ans1 == ans2);
     }
 }
